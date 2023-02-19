b17a353ea4b37bae41b3ba671c549ded96ec64d2
atharvakap <76645846+atharvakap@users.noreply.github.com>
2023-02-19 23:50:11 +0530

    Update mor.py

--- mor.py
+++ mor.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
 import statsmodels.formula.api as smf
+from sklearn.mixture import GaussianMixture
+from patsy import dmatrices
+from flexmix import flexmix, step, refit
+import matplotlib.pyplot as plt
 
 mcdonalds = np.read_csv('mcdonalds.csv')
 counts = pd.value_counts(mcdonalds["Like"])
@@ -16,4 +20,38 @@ f = smf.ols(formula=f)
 formula_str = "Like.n ~ yummy + convenient + spicy + fattening + greasy + fast + cheap + tasty + expensive + healthy + disgusting"
 formula_obj = smf.ols(formula=formula_str)
 
+np.random.seed(1234)
+y, X = dmatrices(str(f), data=mcdonalds, return_type='dataframe')
+gmm = GaussianMixture(n_components=2, n_init=10)
+gmm.fit(X)
+md_reg2 = flexmix(y.values.ravel(), X.values, k=2, cluster=gmm.predict(X))
 
+y, X = dmatrices(str(f), data=mcdonalds, return_type='dataframe')
+np.random.seed(1234)
+model = GaussianMixture(n_components=2, n_init=10)
+search = step(model, X.values, y.values.ravel(), k=2, nrep=10, verb=0)
+md_reg2 = flexmix(y.values.ravel(), X.values, k=search['nclass'], cluster=search['class'])
+
+y, X = dmatrices(str(f), data=mcdonalds, return_type='dataframe')
+np.random.seed(1234)
+model = GaussianMixture(n_components=2, n_init=10)
+search = step(model, X.values, y.values.ravel(), k=2, nrep=10, verb=0)
+md_reg2 = flexmix(y.values.ravel(), X.values, k=search['nclass'], cluster=search['class'])
+md_ref2 = refit(md_reg2)
+print(md_ref2.summary())
+
+y, X = dmatrices(str(f), data=mcdonalds, return_type='dataframe')
+np.random.seed(1234)
+model = GaussianMixture(n_components=2, n_init=10)
+search = step(model, X.values, y.values.ravel(), k=2, nrep=10, verb=0)
+md_reg2 = flexmix(y.values.ravel(), X.values, k=search['nclass'], cluster=search['class'])
+md_ref2 = refit(md_reg2)
+
+# plot the results
+plt.figure()
+plt.scatter(X['Like.n'], y, c=md_ref2.predict(X.values))
+plt.colorbar()
+plt.xlabel('Like.n')
+plt.ylabel('Like')
+plt.title('Cluster plot of McDonald\'s data')
+plt.show()
